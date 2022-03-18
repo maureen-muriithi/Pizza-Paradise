10deb2ea95e94b1432d97e11832c12929a99f4be
Maureen-Muriithi <moh2wanja@gmail.com>
2022-03-18 23:31:55 +0300

    Add toggle function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 $ (document).ready(function(){
-
+    // toggle function on the 'What we do" section, to display description
+    $("#pizza").click(function(){
+        $("#pizzaInfo").toggle();
+        $("#firstImage").toggle();
+    });
+    
+    $("#toppings").click(function(){
+        $("#toppingsInfo").toggle();
+        $("#secondImage").toggle();
+    });
+    
+    $("#crust").click(function(){
+        $("#crustInfo").toggle();
+        $("#thirdImage").toggle();
+    });
     
 })
