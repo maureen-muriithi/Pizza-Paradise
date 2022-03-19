ae1f1865009614b6a06dd1dfb9ac9c7034afa6f9
Maureen-Muriithi <moh2wanja@gmail.com>
2022-03-19 03:00:52 +0300

    Add and define click function to the order button

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $ (document).ready(function(){
     // toggle function on the 'What we do" section, to display description
-    $("#pizza").click(function(){
+    $("#pizzaInfo, #toppingsInfo, #crustInfo").hide();
+
+    $("#pizza-size").click(function(){
         $("#pizzaInfo").toggle();
         $("#firstImage").toggle();
     });
@@ -14,5 +16,22 @@ $ (document).ready(function(){
         $("#crustInfo").toggle();
         $("#thirdImage").toggle();
     });
+
+    $("#orderbutton").click(function(e){
+        e.preventDefault();
+        console.log("Submitted")
+        let sizeOfPizza = $("#pizza-size").find(":selected").val()
+        let toppingsOfPizza = $("#toppings").find(":selected").val()
+        let crustOfPizza = $("#crust").find(":selected").val()
+        console.log("My pizza should be ", sizeOfPizza, "," , crustOfPizza, "and with", toppingsOfPizza)
+
+        let total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
+        let order = 1;
+        let grandTotal = 0;
+        
+    })
+
+    // let Cost = new cost()
     
 })
+
