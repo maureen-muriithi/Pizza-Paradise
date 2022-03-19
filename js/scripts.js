8e73fba4ef387bf93327884fb2eb260c2ea1ab9c
Maureen-Muriithi <moh2wanja@gmail.com>
2022-03-20 02:12:04 +0300

    Add button functions, show and hide more and previos information respectively

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,11 @@ $ (document).ready(function(){
     });
 
     $(".table").hide();
-    $("#addButton").hide();
+    $(".other-buttons").hide();
+    $(".more-info").hide();
+    $("#yes-button").hide();
+    $("#no-button").hide();
+    $(".more-info h4").hide();
 
     $("#orderButton").click(function(e){
         e.preventDefault();
@@ -33,8 +37,25 @@ $ (document).ready(function(){
         let grandTotal = 0;
 
         $(".table").show();
-        $("#addButton").show();
+        $(".other-buttons").show();
         $("#orderButton").hide();
+        $("")
+        
+        $("#size").html($(".size option:selected").text() + " - " + sizeOfPizza);
+        $("#toppings").html($(".toppings option:selected").text() + " - " + toppingsOfPizza);
+        $("#crust").html($(".crust option:selected").text() + " - " + crustOfPizza);
+        $("#total").html(total);
+
+    //     function Pizza(size, toppings, crust, total, orderNo) {
+    //     this.size = size;
+    //     this.toppings = toppings;
+    //     this.crust = crust;
+    //     this.total = total;
+    //     this.orderNo = orderNo;
+    // }
+        
+    
+
         
     })
 
