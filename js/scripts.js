6e4327e619f9572d8bf395aa1b8ce70efeebbc1c
Maureen-Muriithi <moh2wanja@gmail.com>
2022-03-19 03:51:16 +0300

    Add show and hide functions for the tables and buttons

--- js/scripts.js
+++ js/scripts.js
@@ -2,32 +2,39 @@ $ (document).ready(function(){
     // toggle function on the 'What we do" section, to display description
     $("#pizzaInfo, #toppingsInfo, #crustInfo").hide();
 
-    $("#pizza-size").click(function(){
+    $(".first-option").click(function(){
         $("#pizzaInfo").toggle();
         $("#firstImage").toggle();
     });
     
-    $("#toppings").click(function(){
+    $(".second-option").click(function(){
         $("#toppingsInfo").toggle();
         $("#secondImage").toggle();
     });
     
-    $("#crust").click(function(){
+    $(".third-option").click(function(){
         $("#crustInfo").toggle();
         $("#thirdImage").toggle();
     });
 
-    $("#orderbutton").click(function(e){
+    $(".table").hide();
+    $("#addButton").hide();
+
+    $("#orderButton").click(function(e){
         e.preventDefault();
         console.log("Submitted")
-        let sizeOfPizza = $("#pizza-size").find(":selected").val()
-        let toppingsOfPizza = $("#toppings").find(":selected").val()
-        let crustOfPizza = $("#crust").find(":selected").val()
+        let sizeOfPizza = $("#pizza-size").find(":selected").val();
+        let toppingsOfPizza = $("#toppings").find(":selected").val();
+        let crustOfPizza = $("#crust").find(":selected").val();
         console.log("My pizza should be ", sizeOfPizza, "," , crustOfPizza, "and with", toppingsOfPizza)
 
         let total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
         let order = 1;
         let grandTotal = 0;
+
+        $(".table").show();
+        $("#addButton").show();
+        $("#orderButton").hide();
         
     })
 
